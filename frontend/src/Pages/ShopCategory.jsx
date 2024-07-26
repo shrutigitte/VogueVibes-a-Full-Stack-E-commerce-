8edce0960e3d3e099570f8c63edc8ff3e5928005
Shruti Gitte <shrutigitte@gmail.com>
2024-07-27 00:00:09 +0530

    some progress in code
    did some progress in code

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import {ShopContext} from '../Context/ShopContext'
 const ShopCategory = () => {
+  const {all_product}=useContext(ShopContext);
   return (
     <div>
       
