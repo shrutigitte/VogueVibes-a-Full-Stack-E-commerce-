4cab5dfae5b4618ea9d6225fbcdb51f192809b4d
Shruti Gitte <shrutigitte@gmail.com>
2024-08-13 23:14:04 +0530

    Attached links

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
     return (
         <div className='flex justify-around p-5'>
             <div >  
-             <p id='logo' className=' -ml-2 text-lg lg:text-5xl mr-2 font-medium'>VogueVibes</p>
+             <p id='logo' className=' -ml-2 text-lg lg:text-5xl mr-2 font-medium'><Link to='/'>VogueVibes</Link> </p>
             </div>
             
              
@@ -22,7 +22,7 @@ const NavBar = () => {
                 <li onClick={()=>setMenu("kids")}> <Link to='/kids'>Kids</Link> {menu==="kids" ? <hr />:<></>}</li>
             </ul>
             <div className='flex'>
-                <Link to='/loginsingup'><button className='lg:h-10 lg:w-20 h-7 w-12 -mt-1 ml-2 border border-teal-500 rounded-2xl p-1 bg-teal-600 hover:bg-teal-500 active:bg-teal-400 text-white font-medium' >Login</button></Link> 
+                <Link to='/loginsignup'><button className='lg:h-10 lg:w-20 h-7 w-12 -mt-1 ml-2 border border-teal-500 rounded-2xl p-1 bg-teal-600 hover:bg-teal-500 active:bg-teal-400 text-white font-medium' >Login</button></Link> 
                 <Link to='/cart'> <img className="lg:ml-3 lg:mt-1 " alt='cart' height={30} width={45} src={cart_icon}/></Link> 
                 <div className="w-5 h-6 flex mt-[-37] justify-center -ml-3 bg-red-500 pb-1 text-white rounded-xl">{getTotalCartItems()}</div>
             </div>
